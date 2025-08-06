5f52325e61d686e1e62f32d97598c2a62d2d855e
Ashitori-ctrl <tigr.box17@gmail.com>
2025-08-06 15:46:46 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ public:
     virtual void speak() const {
         cout << "Животное издает звук" << endl;
     }
-    virtual ~Animal() = default;
+    virtual ~Animal() = default; // Я не знаю точно, тут будет корректно удаляться или нет. https://ru.stackoverflow.com/questions/240150/%D0%97%D0%B0%D1%87%D0%B5%D0%BC-%D0%BD%D1%83%D0%B6%D0%B5%D0%BD-%D0%B2%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B4%D0%B5%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D0%BE%D1%80-%D0%BF%D1%80%D0%B8-%D0%BD%D0%B0%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B8 вот по этой ссылке тут взял
 };
 
 class Dog : public Animal {
